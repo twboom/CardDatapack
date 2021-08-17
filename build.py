4a2896296d7fb0c5d81aa42112cb5c4bbefc7d88
Thijs Boom <twboom@hotmail.nl>
2021-08-17 23:00:26 +0200

    Made buildscript better

--- build.py
+++ build.py
@@ -1,4 +1,15 @@
 import shutil
 import argparse
 
-shutil.make_archive("cardmaker", "zip", "src")
+parser = argparse.ArgumentParser(description='Build a new release.')
+parser.add_argument('-v', '--version', help='The version to build.')
+
+args = parser.parse_args()
+
+if not args.version:
+    print('Please specify a version to build.')
+    exit(1)
+
+print(f'Starting build of CardMaker with version {args.version}')
+shutil.make_archive(f'CardMaker-{args.version}', "zip", "src")
+print(f'Finished build')
